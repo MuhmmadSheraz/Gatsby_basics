8f055fac817a93fa52425d40e181d0516cbebbd3
MuhammdSheraz <mshazy999@gmail.com>
2021-01-14 01:42:18 +0500

    navigation and linking added

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import styles from "./navbar.module.css"
+import styles from "./navbar.module.css";
+import { Link, navigate } from "gatsby";
 const index = () => {
   return (
     <div className={styles.navbar}>
-      <p>Home</p>
-      <p>About Us</p>
-      <p>Contact</p>
+      {/* This is Link  Navigate Method  */}
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+      {/* This is OnClick Navigate Method  */}
+      <p onClick={() => navigate("/about")}>About Us</p> 
+      <p onClick={() => navigate("/contact")}>Contact </p>
     </div>
   );
 };
