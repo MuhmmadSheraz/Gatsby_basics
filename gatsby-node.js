2f42b9a0262d5ee05101de522e3dc3647b0d059a
MuhammdSheraz <mshazy999@gmail.com>
2021-01-14 10:48:57 +0500

    material ui added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,20 @@
 exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   // Only update the `/app` page.
   if (page.path.match(/^\/app/)) {
-    // page.matchPath is a special key that's used for matching pages
-    // with corresponding routes only on the client.
-    page.matchPath = "/app/*"
+    page.matchPath = "/app/*";
 
     // Update the page.
-    createPage(page)
+    createPage(page);
   }
   const user = [
     { name: "Hamza", email: "Hamza@gmail.com" },
     { name: "Ali", email: "Ali32@gmail.com" },
+  ];
+  const Fuser = [
+    { name: "Mark", email: "Mark@gmail.com" },
+    { name: "John", email: "Ali32@gmail.com" },
   ];
   actions.createPage({
     path: "localuser",
@@ -21,12 +23,7 @@ exports.onCreatePage = async ({ page, actions }) => {
       data: user,
     },
   });
-  console.log("End of Gatsby Node File");
 
-  const Fuser = [
-    { name: "Mark", email: "Mark@gmail.com" },
-    { name: "John", email: "Ali32@gmail.com" },
-  ];
   actions.createPage({
     path: "foriegnUser",
     component: require.resolve(`./src/Templates/dynamic-page.tsx`),
@@ -34,5 +31,4 @@ exports.onCreatePage = async ({ page, actions }) => {
       data: Fuser,
     },
   });
-  console.log("End of Gatsby Node File");
-}
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,30 @@ import { navigate } from "@reach/router";
 import React from "react";
 import Content from "../components/Content";
 import Layout from "../components/Layout";
-
+import Button from "@material-ui/core/Button";
+import { Box } from "@material-ui/core";
 export default () => {
   return (
     <Layout>
       <div className="page-Wrapper">
         <h1 className="pageHeading">Hello I Am Index Page of Gatsby Website</h1>{" "}
         <Content data={"This Is Index Page And The Content Will be Dynamic"} />
-        <button onClick={() => navigate("/app/client-data-one")}>
-          Go to My data-1
-        </button>
-        <button onClick={() => navigate("/app/client-data-two")}>
-          Go to My data-2
-        </button>
+        <div className="div-center">
+          <Button
+            onClick={() => navigate("/app/client-data-one")}
+            variant="contained"
+            color="primary"
+          >
+            Go to My data-1
+          </Button>
+          <Button
+            onClick={() => navigate("/app/client-data-two")}
+            variant="contained"
+            color="secondary"
+          >
+            Go to My data-2
+          </Button>
+        </div>
       </div>
     </Layout>
   );
