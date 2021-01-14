9a136d8d73559998f0299bbd6691db5ff482555c
MuhammdSheraz <mshazy999@gmail.com>
2021-01-14 10:21:31 +0500

    fixed gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,4 +10,29 @@ exports.onCreatePage = async ({ page, actions }) => {
     // Update the page.
     createPage(page)
   }
+  const user = [
+    { name: "Hamza", email: "Hamza@gmail.com" },
+    { name: "Ali", email: "Ali32@gmail.com" },
+  ];
+  actions.createPage({
+    path: "localuser",
+    component: require.resolve(`./src/Templates/dynamic-page.tsx`),
+    context: {
+      data: user,
+    },
+  });
+  console.log("End of Gatsby Node File");
+
+  const Fuser = [
+    { name: "Mark", email: "Mark@gmail.com" },
+    { name: "John", email: "Ali32@gmail.com" },
+  ];
+  actions.createPage({
+    path: "foriegnUser",
+    component: require.resolve(`./src/Templates/dynamic-page.tsx`),
+    context: {
+      data: Fuser,
+    },
+  });
+  console.log("End of Gatsby Node File");
 }
