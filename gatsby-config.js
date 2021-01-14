447eda9c7a05ecd8c1cb955d24151f295520f568
MuhammdSheraz <mshazy999@gmail.com>
2021-01-14 22:47:20 +0500

    dynamic page rendering with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: ["gatsby-plugin-typescript", `gatsby-plugin-material-ui`],
+  plugins: [
+    "gatsby-plugin-typescript",
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `gt2z1iv64oav`,
+        accessToken: `R-fyDGrqZI76UGL8tUqh9Nc3sq79GWwyR6eVd3X7X_8`,
+      },
+    },
+  ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,37 @@
-exports.onCreatePage = async ({ page, actions }) => {
+const path = require("path");
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
 
-  // Only update the `/app` page.
-  if (page.path.match(/^\/app/)) {
-    page.matchPath = "/app/*";
+  const result = await graphql(`
+    {
+      allContentfulGatsbyBasics {
+        nodes {
+          titlle
+          updatedAt
+          blogImage {
+            title
+            updatedAt
+            file {
+              url
+            }
+          }
+          blogContent {
+            raw
+          }
+        }
+      }
+    }
+  `);
+  result.data.allContentfulGatsbyBasics.nodes.forEach((item) => {
+    console.log("Item ===>", item.blogImage.file.url);
 
-    // Update the page.
-    createPage(page);
-  }
-  const user = [
-    { name: "Hamza", email: "Hamza@gmail.com" },
-    { name: "Ali", email: "Ali32@gmail.com" },
-  ];
-  const Fuser = [
-    { name: "Mark", email: "Mark@gmail.com" },
-    { name: "John", email: "Ali32@gmail.com" },
-  ];
-  actions.createPage({
-    path: "localuser",
-    component: require.resolve(`./src/Templates/dynamic-page.tsx`),
-    context: {
-      data: user,
-    },
+    createPage({
+      path: `/blogs/${item.titlle}`,
+      component: path.resolve("./src/Templates/Blogs.tsx"),
+      context: {
+        blogData: item,
+      },
+    });
   });
-
-  actions.createPage({
-    path: "foriegnUser",
-    component: require.resolve(`./src/Templates/dynamic-page.tsx`),
-    context: {
-      data: Fuser,
-    },
-  });
+  console.log(JSON.stringify(result));
 };
